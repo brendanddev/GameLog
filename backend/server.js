4c7fb579cc667e71f8858a421ea04d714c72b2d8
Brendan Dileo <dileob23@gmail.com>
2025-04-15 10:36:07 -0400

    Setup Express Server

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,21 @@
 /**
  * @file server.js
  * @author Brendan Dileo, April 2025
+ * 
+ * Setup for the Express server and SQLite database.
  */
+
+// Express for creating the server and handling requests
+const express = require('express');
+
+// Enables CORS
+const cors = require('cors');
+
+
+// Express app setup and port number initialization
+const app = express();
+const port = 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
