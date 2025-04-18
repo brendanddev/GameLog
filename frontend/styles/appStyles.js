17625904d0a36baf5924659f0df63c864d13e6ab
Brendan Dileo <dileob23@gmail.com>
2025-04-18 12:05:43 -0400

    Create Styles Directory and Main File

---
+++ frontend/styles/appStyles.js
@@ -0,0 +1,14 @@
+
+/**
+ * @file appStyles.js
+ * @author Brendan Dileo, April 2025
+ * 
+ * The styles for the App.
+ *
+ * StAuth10244: I Brendan Dileo, 000879513 certify that this material is my original work.
+ * No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+ */
+
+import { StyleSheet } from 'react-native';
+
+const appStyles = StyleSheet.create({
