e5a2dad7299387132758c797ef8365905c7d09aa
Brendan Dileo <dileob23@gmail.com>
2025-04-18 15:57:34 -0400

    Setup Frontend Styling

---
+++ frontend/styles/collectionStyles.js
@@ -0,0 +1,16 @@
+
+/**
+ * @file collectionStyles.js
+ * @author Brendan Dileo, April 2025
+ * 
+ * The styles for the Collection Screen Component of the Gamelog App.
+ *
+ * StAuth10244: I Brendan Dileo, 000879513 certify that this material is my original work.
+ * No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+ */
+
+import { StyleSheet } from 'react-native';
+
+export const collectionStyles = StyleSheet.create({
+
+}); 
---
+++ frontend/styles/homeStyles.js
@@ -0,0 +1,16 @@
+
+/**
+ * @file homeStyles.js
+ * @author Brendan Dileo, April 2025
+ * 
+ * The styles for the Home Screen of the Gamelog App.
+ *
+ * StAuth10244: I Brendan Dileo, 000879513 certify that this material is my original work.
+ * No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+ */
+
+import { StyleSheet } from 'react-native';
+
+export const homeStyles = StyleSheet.create({
+
+}); 
